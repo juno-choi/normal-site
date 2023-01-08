2557aaf75ca6b7e26080a312af1161ea02e0701c
juno-choi <ililil9482@naver.com>
2023-01-08 16:51:12 +0900

    [추가] 프로젝트 세팅

---
+++ src/Header.svelte
@@ -0,0 +1,16 @@
+<div class="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+    <div class="flex justify-start lg:w-0 lg:flex-1">
+    <a href="/">
+        <span class="sr-only">Your Company</span>
+        <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+    </a>
+    </div>
+    <nav class="space-x-10 md:flex">
+    <a href="/" class="text-base font-medium text-gray-500 hover:text-gray-900">Home</a>
+    <a href="/about" class="text-base font-medium text-gray-500 hover:text-gray-900">About</a>
+    <a href="/board" class="text-base font-medium text-gray-500 hover:text-gray-900">Board</a>
+    </nav>
+    <div class="items-center justify-end md:flex md:flex-1 lg:w-0">
+    <a href="/login" class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">로그인</a>
+    </div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Header from '../Header.svelte';
+  	import "../styles.css";
+</script>
+<div>
+	<div class="relative bg-white">
+	<div class="mx-auto max-w-7xl px-6">
+			<Header />
+			<slot />
+		</div>
+	</div>
+</div>
